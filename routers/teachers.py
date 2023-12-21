02cca58b02078505e1d757a4e297babe22139155
alx_27 <didieralejandromejia83@gmail.com>
2023-12-20 21:05:27 -0500

    endpoint for delete a teacher and some fixes

--- routers/teachers.py
+++ routers/teachers.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, status, HTTPException
 from db.client import db_client
 from schemas.teachers import teachers_schema, teacher_schema
 from models.teacher import Teacher
+from bson import ObjectId
 
 router = APIRouter(prefix="/teachers",
                    tags=["teachers"],
@@ -19,3 +20,10 @@ async def post_teacher(teacher: Teacher):
     id = db_client.teachers.insert_one(teacher_dict).inserted_id
     
     return teacher_schema(db_client.teachers.find_one({"_id": id}))
+
+@router.delete("/v1/delete_teacher", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_teacher(id: str):
+    try:
+        db_client.teachers.find_one_and_delete({"_id": ObjectId(id)})
+    except:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Teacher not found")
