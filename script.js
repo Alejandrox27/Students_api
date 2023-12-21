42ec93380f434f0f3fb6aa7b0cbef72c9a25b7af
alx_27 <didieralejandromejia83@gmail.com>
2023-12-21 11:54:19 -0500

    Form for students and teachers

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ async function loaded(){
         .then(res => res.json())
         .then(data => localStorage.setItem("Students" ,JSON.stringify(data)))
         .catch((e) => console.log("no data on localStorage"))
+
+    await fetch("http://127.0.0.1:8000/teachers/v1/get-teachers")
+        .then(res => res.json())
+        .then(data => localStorage.setItem("Teachers", JSON.stringify(data)))
+        .catch((e) => console.log("no data on localStorage"))
 }
 
 window.addEventListener("load", loaded, false);
