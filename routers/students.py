b9bccfb6359df48cedcd50a9b75b99e0b1e6e2f3
alx_27 <didieralejandromejia83@gmail.com>
2023-12-20 20:33:24 -0500

    endpoint for delete student

--- routers/students.py
+++ routers/students.py
@@ -9,11 +9,11 @@ router = APIRouter(prefix="/students",
                    tags=["students"],
                    responses={404: {status.HTTP_404_NOT_FOUND: "not found"}})
 
-@router.get("/v1/get", response_model=list[Student], status_code=status.HTTP_200_OK)
+@router.get("/v1/get_students", response_model=list[Student], status_code=status.HTTP_200_OK)
 async def get_students():
     return users_schema(db_client.students.find())
 
-@router.post("/v1/post", response_model=Student, status_code=status.HTTP_201_CREATED)
+@router.post("/v1/post_student", response_model=Student, status_code=status.HTTP_201_CREATED)
 async def post_student(student: Student):
     dict_student = student.dict()
     del dict_student["id"]
@@ -24,11 +24,21 @@ async def post_student(student: Student):
     
     return user_schema(student)
 
-@router.patch("/v1/update_grades", status_code=status.HTTP_200_OK)
+@router.patch("/v1/update_grades", response_model=dict, status_code=status.HTTP_200_OK)
 async def update_grades(student: Student_grades):
     student_dict = student.dict(exclude_unset=True)
     del student_dict["id"]
     
-    return user_schema((db_client.students.find_one_and_update({"_id": ObjectId(student.dict(exclude_unset=True)["id"])}, 
+    try:
+        return user_schema((db_client.students.find_one_and_update({"_id": ObjectId(student.dict(exclude_unset=True)["id"])}, 
                                             {"$set": student_dict},
                                             return_document=ReturnDocument.AFTER)))
+    except:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="student not found")
+    
+@router.delete("/v1/delete_student", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_student(id: str):
+    try:
+        db_client.students.find_one_and_delete({"_id": ObjectId(id)})
+    except:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="student not found")
